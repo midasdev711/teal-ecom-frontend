e4d680f5d8bf6770d67acab60cd27c9cd9639187
Vimal Tagline <vimal.tagline@gmail.com>
2020-09-30 19:09:49 +0530

    url change

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
- // uri: 'https://api.teal.com/graphQL',
+    uri: 'https://api.teal.com/graphQL',
   // uri: 'http://localhost:3010/ecomm/graphql',
-  uri:"http://localhost:9200/ecom/graphql",
+  //uri:"http://localhost:9200/ecom/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Article: {
